bf11218d431ec9f381c1718e7ae523bc47563352
Hashmita Mundhda <142114260+HashmitaMundhda@users.noreply.github.com>
2023-09-10 15:26:43 -0700

    Add files via upload

---
+++ IT/Java/SnakeAndLadderGame/driver.java
@@ -0,0 +1,71 @@
+package bootstrap;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import domain.*;
+import org.apache.log4j.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class driver {
+    public static Logger logger = LoggerFactory.getLogger(driver.class);
+    private static dice die = new dice();
+    public static void main(String[] args) throws InterruptedException {
+        configureLogging("game_final_result.log", "INFO");
+        logger.info("*********************************Welcome to Snakes & Ladders by Hashmita Mundhda******************************");
+        int noOfPlayer = 4;
+        logger.info(noOfPlayer+" players are playing " );
+
+        List<Player> players = new ArrayList();
+        Player player1 = new Player(("Hashmita"));
+        players.add(player1);
+        Player player2 = new Player(("Dharmesh"));
+        players.add(player2);
+        Player player3 = new Player(("Yashita"));
+        players.add(player3);
+        Player player4 = new Player(("Chetna"));
+        players.add(player4);
+        board board = new board(players);
+
+        boolean Finish = false;
+        int playerId = 0;
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        while (!Finish){
+            Player currPlayer = players.get(playerId);
+            List<Future<Integer>> roll = executorService.invokeAll(players);
+            Finish = board.movePlayer(currPlayer, roll);
+            if (Finish){
+                logger.info(currPlayer + " Wins!");
+            }
+            playerId++;
+            if (playerId == noOfPlayer){
+                playerId = 0;
+            }
+        }
+    }
+    public static String configureLogging(String logFile, String logLevel) {
+        DailyRollingFileAppender dailyRollingFileAppender = new DailyRollingFileAppender();
+        switch (logLevel) {
+            case "DEBUG": {
+                dailyRollingFileAppender.setThreshold(Level.toLevel(Priority.DEBUG_INT));
+            }
+            case "WARN": {
+                dailyRollingFileAppender.setThreshold(Level.toLevel(Priority.WARN_INT));
+            }
+            case "ERROR": {
+                dailyRollingFileAppender.setThreshold(Level.toLevel(Priority.ERROR_INT));
+            }
+            default: {
+                dailyRollingFileAppender.setThreshold(Level.toLevel(Priority.INFO_INT));
+            }
+            break;
+        }
+        System.out.println("Log files written out at " + logFile);
+        dailyRollingFileAppender.setFile(logFile);
+        dailyRollingFileAppender.setLayout(new EnhancedPatternLayout("%d [%t] %-5p %c - %m%n"));
+        dailyRollingFileAppender.activateOptions();
+        org.apache.log4j.Logger.getRootLogger().addAppender(dailyRollingFileAppender);
+        return dailyRollingFileAppender.getFile();
+    }
+}
